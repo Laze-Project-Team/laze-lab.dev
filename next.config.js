f37aeaebb821e85d43f8a5dfae26340e80ffa11e
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-19 08:47:46 +0900

    🚧 set up "next-i18next"

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const { i18n } = require('./next-i18next.config');
+
+const localeSubpaths = {};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  // next-i18next
+  i18n,
+  publicRuntimeConfig: {
+    localeSubpaths,
+  },
 };
 
 module.exports = nextConfig;
