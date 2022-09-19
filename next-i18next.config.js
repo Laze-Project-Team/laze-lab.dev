fc0f127f1437d35cc723f6a98d9b72ce0f0dec1c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-19 10:53:42 +0900

    🚧 configure "next-i18next"

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,13 @@
+/**
+ * @type {import('next-i18next').UserConfig}
+ */
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
   },
   localePath: './src/locales/',
+
+  // reload locale file while "yarn dev"
+  reloadOnPrerender: true,
 };
