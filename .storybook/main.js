2de4f47e5241a150be50d9ea3288a3d49fee596c
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-20 17:54:23 +0900

    🔧 configure Storybook to run on Next.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -8,5 +10,18 @@ module.exports = {
   framework: '@storybook/react',
   core: {
     builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      path: false,
+    };
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../src'),
+      '/': path.resolve(__dirname, '../'),
+    };
+
+    return config;
   },
 };
