2d062d1517c48e2d64b2f496d7c91480ac903c1e
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-21 22:07:23 +0900

    🚧 make _app.tsx providers and global styles work on Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,15 @@
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+import { MyApp } from '../src/pages/_app';
+
+export const decorators = [
+  (Story) => (
+    <I18nextProvider i18n={i18n}>
+      <MyApp Component={Story}></MyApp>
+    </I18nextProvider>
+  ),
+];
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,19 @@ import 'ress';
 
 import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import type { SSRConfig } from 'next-i18next';
 import { appWithTranslation } from 'next-i18next';
+import type { ComponentType } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { store } from '@/lib/redux/root';
 import { globalStyle } from '@/styles/global';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
+export const MyApp: ComponentType<
+  AppProps & {
+    pageProps: SSRConfig;
+  }
+> = ({ Component, pageProps }) => {
   return (
     <>
       <ReduxProvider store={store}>
