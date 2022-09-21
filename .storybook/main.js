b56907275eb2959344878e3770bf8eacf431ce76
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-21 22:52:00 +0900

    🔧 makes Storybook reload faster

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  typescript: { reactDocgen: 'react-docgen' },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
