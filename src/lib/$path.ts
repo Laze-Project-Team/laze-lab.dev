3905ffe2aa46d74357832924ed3ae9e6101d5cfd
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-22 17:51:54 +0900

    🚧 update $path.ts

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -6,6 +6,26 @@ export type PagesPath = typeof pagesPath
 
 export const staticPath = {
   favicon_ico: '/favicon.ico',
+  favicons: {
+    android_chrome_192x192_png: '/favicons/android-chrome-192x192.png',
+    android_chrome_384x384_png: '/favicons/android-chrome-384x384.png',
+    apple_touch_icon_png: '/favicons/apple-touch-icon.png',
+    browserconfig_xml: '/favicons/browserconfig.xml',
+    favicon_16x16_png: '/favicons/favicon-16x16.png',
+    favicon_32x32_png: '/favicons/favicon-32x32.png',
+    favicon_ico: '/favicons/favicon.ico',
+    mstile_150x150_png: '/favicons/mstile-150x150.png',
+    safari_pinned_tab_svg: '/favicons/safari-pinned-tab.svg',
+    site_webmanifest: '/favicons/site.webmanifest'
+  },
+  img: {
+    logo: {
+      logo_png: '/img/logo/logo.png',
+      logo_caption_png: '/img/logo/logo_caption.png',
+      logo_caption_gray_png: '/img/logo/logo_caption_gray.png',
+      logo_gray_png: '/img/logo/logo_gray.png'
+    }
+  },
   vercel_svg: '/vercel.svg'
 } as const
 
