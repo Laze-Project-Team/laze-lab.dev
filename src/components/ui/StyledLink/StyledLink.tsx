cb1e13adf7c516a48e0bf68f022987acabb59408
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-24 23:26:09 +0900

    🎨 fix the icon of StyledLink

--- src/components/ui/StyledLink/StyledLink.tsx
+++ src/components/ui/StyledLink/StyledLink.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-no-target-blank */
+import { css } from '@emotion/react';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import Link from 'next/link';
 import type { ComponentPropsWithRef, FC } from 'react';
@@ -14,6 +15,7 @@ export const PresentialStyledLink: FC<presentialStyledLinkProps> = ({
   href,
   children,
   disableIcon,
+  css: cssProp,
   ...props
 }) => {
   const isInternal = href.startsWith('/') || href.startsWith('#');
@@ -25,10 +27,25 @@ export const PresentialStyledLink: FC<presentialStyledLinkProps> = ({
           href={href}
           target={isInternal ? undefined : '_blank'}
           rel={isInternal ? undefined : 'noopener noreferrer'}
+          css={[
+            cssProp,
+            css`
+              display: inline-flex;
+              align-items: center;
+            `,
+          ]}
           {...props}
         >
           {children}
-          {!disableIcon && !isInternal && <OpenInNewIcon />}
+          {!disableIcon && !isInternal && (
+            <OpenInNewIcon
+              css={css`
+                width: 1rem;
+                height: 1rem;
+                margin-left: 0.25rem;
+              `}
+            />
+          )}
         </a>
       </Link>
     </>
