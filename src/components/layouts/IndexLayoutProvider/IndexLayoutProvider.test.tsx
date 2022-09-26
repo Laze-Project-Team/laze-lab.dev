4984ef23873396e4d9be69bf628bb80059471ff1
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-09-26 23:47:47 +0900

    ✅ change test in IndexLayoutProvider

--- src/components/layouts/IndexLayoutProvider/IndexLayoutProvider.test.tsx
+++ src/components/layouts/IndexLayoutProvider/IndexLayoutProvider.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
 
-import { screen } from '@testing-library/react';
 import { expect, it } from 'vitest';
 
 import { render } from '@/lib/test/render';
@@ -10,9 +9,4 @@ import { IndexLayoutProvider } from '.';
 it('should be rendered correctly', () => {
   const { container } = render(<IndexLayoutProvider />);
   expect(container.firstChild).toMatchSnapshot();
-});
-
-it('should have "???" role', () => {
-  render(<IndexLayoutProvider />);
-  expect(screen.getByRole('???')).toBeInTheDocument();
 });
