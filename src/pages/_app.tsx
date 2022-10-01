afdaeb9b60f12774ceee1ee282df079d8773ee4b
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-10-01 19:39:57 +0900

    ✨ introduce nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import 'ress';
+import 'nprogress/nprogress.css';
 
 import { Global } from '@emotion/react';
 import type { PaletteMode } from '@mui/material';
 import type { AppProps as NextAppProps } from 'next/app';
 import type { SSRConfig } from 'next-i18next';
 import { appWithTranslation } from 'next-i18next';
+import nprogress from 'nprogress';
 import type { ComponentType } from 'react';
+import { useEffect } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import { ColorModeProvider } from '@/components/contexts/ColorModeContext';
@@ -22,6 +25,14 @@ export const MyApp: ComponentType<AppProps> = ({
   pageProps,
   preferTheme,
 }) => {
+  if (typeof window !== 'undefined') {
+    nprogress.start();
+  }
+
+  useEffect(() => {
+    nprogress.done();
+  });
+
   return (
     <>
       <ReduxProvider store={store}>
