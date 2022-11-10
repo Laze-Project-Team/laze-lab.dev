3002cd311fba0a4c2859ffd4ce2e084ca7ab6eac
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-11-10 19:51:52 +0900

    🚧 fix storybook error
    - do not import 'vitest' in storybook component (because vitest import other standard library of node.js)

--- __mocks__/swr.ts
+++ __mocks__/swr.ts
@@ -1,8 +1,7 @@
 import type { SWRResponse } from 'swr';
-import { vi } from 'vitest';
 
 export const mockSWRResponse = <T>(data: T): SWRResponse<T> => ({
   isValidating: false,
-  mutate: vi.fn(),
+  mutate: async () => void 0,
   data,
 });
