26f700385b633d15b30eeeb78ccdfeae3752bb83
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-04 16:57:27 +0900

    🚧 use google fonts through <link> tag instead of next/font/google because of performance issue (compiling takes too long time)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import 'nprogress/nprogress.css';
 import { Global } from '@emotion/react';
 import type { PaletteMode } from '@mui/material';
 import type { AppProps as NextAppProps } from 'next/app';
-import { Noto_Sans_JP as notoSansJP } from 'next/font/google';
 import type { SSRConfig } from 'next-i18next';
 import { appWithTranslation } from 'next-i18next';
 import nprogress from 'nprogress';
@@ -16,12 +15,6 @@ import { ColorModeProvider } from '@/components/contexts/ColorModeContext';
 import { ToastContainer } from '@/components/functional/ToastContainer';
 import { store } from '@/lib/redux/root';
 import { globalStyle } from '@/styles/global';
-
-const fontFamily = notoSansJP({
-  subsets: ['latin'],
-  weight: ['400'],
-  fallback: ['Roboto', 'Helvetica', 'Arial', 'sans-serif'],
-});
 
 type AppProps = NextAppProps & {
   pageProps: SSRConfig;
@@ -47,7 +40,7 @@ export const MyApp: ComponentType<AppProps> = ({
         <ColorModeProvider preferTheme={preferTheme}>
           <ToastContainer />
           <Global styles={globalStyle} />
-          <div className={fontFamily.className}>
+          <div>
             <Component {...pageProps} />
           </div>
         </ColorModeProvider>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,11 @@ const Document: ComponentType<DocumentProps> = () => {
     <Html>
       <Head nonce={nonce}>
         <meta httpEquiv="Content-Security-Policy" content={csp} />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
